125b61afba8e11fa459eb3ddcf9889146837b5a3
Dipanshu <dipanshupandey4488@gmail.com>
2026-04-14 23:57:28 +0530

    added feed api

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const userRouter = express.Router();
 const userAuth = require("../middlewares/auth");
 const connectionRequestModel = require("../models/connectionRequest");
+const userModel=require("../models/user");
 
 userRouter.get("/user/requests/interested", userAuth, async (req, res) => {
     try {
@@ -20,22 +21,44 @@ userRouter.get("/user/requests/interested", userAuth, async (req, res) => {
 
 userRouter.get("/user/connections", userAuth, async (req, res) => {
     try {
-        const user=req.user;
-        const connections=await connectionRequestModel.find({
-            $or:[
-                {fromId:user._id},
-                {toId:user._id}
+        const user = req.user;
+        const connections = await connectionRequestModel.find({
+            $or: [
+                { fromId: user._id },
+                { toId: user._id }
             ],
-            status:"matched"
-        }).populate("fromId",["firstName", "lastName", "age", "gender", "about", "skills", "photoURL"]).populate("toId",["firstName", "lastName", "age", "gender", "about", "skills", "photoURL"]);
-        const data=connections.map(connection=>connection.fromId._id.equals(user._id)?connection.toId:connection.fromId);
+            status: "matched"
+        }).populate("fromId", ["firstName", "lastName", "age", "gender", "about", "skills", "photoURL"]).populate("toId", ["firstName", "lastName", "age", "gender", "about", "skills", "photoURL"]);
+        const data = connections.map(connection => connection.fromId._id.equals(user._id) ? connection.toId : connection.fromId);
         // res.send(data);
-        return res.json({data:data});
+        return res.json({ data: data });
     } catch (error) {
         return res.status(500).json({
-            message:"Something went wrong!",
-            error:error.message
+            message: "Something went wrong!",
+            error: error.message
         });
+    }
+});
+
+userRouter.get("/user/feed", userAuth,async (req, res) => {
+    try {
+        const user=req.user;
+        let hiddenUsers = [];
+        const connections = await connectionRequestModel.find(
+            {
+                $or: [
+                    { fromId: user._id },
+                    { toId: user._id }
+                ]
+            }).select(["fromId","toId"]);
+
+        const connectionsIds=connections.map((item)=>item.fromId.equals(user._id)?item.toId:item.fromId);
+        hiddenUsers=[...connectionsIds,user._id];
+        const feed=await userModel.find({_id:{$nin:hiddenUsers}}).select(["firstName","lastName","age","about","gender","skills"]);
+        
+        return res.send(feed);
+    } catch (error) {
+        return res.status(400).json({ message: "something went wrong!" ,  error: error });
     }
 });
 
